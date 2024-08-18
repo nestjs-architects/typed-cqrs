b5d03567386cf811034b4b2d5a006483809f2add
ZimGil <gil.tichon@gmail.com>
2024-08-19 00:05:02 +0300

    feat: Support for bus interfaces

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,28 @@ declare module '@nestjs/cqrs/dist/query-bus' {
   export interface QueryBus {
     execute<X>(query: Query<X>): Promise<X>;
   }
-
-  export type IInferredQueryHandler<QueryType extends Query<unknown>> = QueryType extends Query<
-    infer ResultType
-  >
-    ? IQueryHandler<QueryType, ResultType>
-    : never;
 }
 
 declare module '@nestjs/cqrs/dist/command-bus' {
   export interface CommandBus {
     execute<X>(command: Command<X>): Promise<X>;
   }
+}
 
+declare module '@nestjs/cqrs/dist/interfaces/queries/query-bus.interface' {
+  export interface IQueryBus {
+    execute<X>(query: Query<X>): Promise<X>;
+  }
+  export type IInferredQueryHandler<QueryType extends Query<unknown>> = QueryType extends Query<
+    infer ResultType
+  >
+    ? IQueryHandler<QueryType, ResultType>
+    : never;
+}
+declare module '@nestjs/cqrs/dist/interfaces/commands/command-bus.interface' {
+  export interface ICommandBus {
+    execute<X>(command: Command<X>): Promise<X>;
+  }
   export type IInferredCommandHandler<
     CommandType extends Command<unknown>
   > = CommandType extends Command<infer ResultType>
